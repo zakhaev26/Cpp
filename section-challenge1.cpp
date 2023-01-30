62cab8a55cc73f51c24d771077c65fb9d0000737
Soubhik <b422056@iiit-bh.ac.in>
2023-01-30 14:02:00 +0530

    new commits

---
+++ section-challenge1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int fav_num;
+    std::cout<<"Enter your fav number between 1-100\n";
+    std::cin>>fav_num;
+    std::cout<<"Amazing,Thats my fav number too!"<<std::endl;
+    std::cout<<"No Really! "<<fav_num<<" is my fav number too!";
+return 0;
+}
