61330a701b0a77514a403c3dedc43f97f2fef93b
Soubhik <b422056@iiit-bh.ac.in>
2023-01-31 20:26:41 +0530

    updates

---
+++ sizeof.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+
+cout<<"int ="<<sizeof(int)<<endl;
+
+cout<<"float ="<<sizeof(int)<<endl;
+
+cout<<"long ="<<sizeof(long)<<endl;
+
+cout<<"long long ="<<sizeof(long long)<<endl;
+
+
+cout<<"min val char = "<<CHAR_MIN<<endl;
+cout<<"min val INT = "<<SHRT_MIN<<endl;
+cout<<"min val LONG = "<<LONG_MIN<<endl;
+cout<<"min val LLONG = "<<LLONG_MIN<<endl;
+
+
+
+
+    return 0;
+}
