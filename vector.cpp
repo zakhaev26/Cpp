b8210b0bf55a83b6fc11016d23183ef5db215092
Soubhik <b422056@iiit-bh.ac.in>
2023-02-03 22:36:25 +0530

    updates

--- vector.cpp
+++ vector.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
-#include<>
-
+#include<vector>
+using namespace std;
 
 int main(){
+//declaring
+vector <int> number {23,34,45,65,67,78,90};
 
 
 
-    
+
+//pushback
+cout<<number.at(2)<<endl;
+number.push_back(777);
+cout<<number[7];
+
+
+
+    return 0;
 }
