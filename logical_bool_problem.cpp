be45319bc80599334442769f2665a0aa8edafab1
Soubhik <b422056@iiit-bh.ac.in>
2023-02-05 01:35:21 +0530

    update

--- logical_bool_problem.cpp
+++ logical_bool_problem.cpp
@@ -15,7 +15,7 @@ int main(){
 
     cin>>userint;
 
-    bool result =(userint>=bound_lower)&&(userint<bound_upper);
+    bool result =(userint>=bound_lower)&&(userint<bound _upper);
 
     cout<<"Is "<<userint<<" within range = "<<result;
 
